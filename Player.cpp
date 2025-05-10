7043721423bd27ebbfe7ffdeedfdaac5505361ea
rlarjsl03 <405zign@gmail.com>
2025-05-10 23:51:28 +0900

    Commit

--- Player.cpp
+++ Player.cpp
@@ -1,56 +1,59 @@
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <iostream>
+#include <SFML/Window.hpp>  
+#include <SFML/Graphics.hpp>  
+#include <iostream>  
 
 using namespace sf;
 
-class Player
-{
-private:
-	CircleShape shape;
+class Game {  
+public:  
+  RenderWindow _window;
+  CircleShape shape; // Declare shape as a member variable
 
-public:
-	Player(CircleShape shape)
-	{
-		this->shape = shape;
-	}
-	void move(Keyboard a)
-	{
-		if (Keyboard::isKeyPressed(Keyboard::Left) == true)
-		{
-			shape.move(30.0f, 0.0f);
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Right) == true) {
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Up) == true) {
-			shape.move(0.0f, -30.0f);
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Down) == true) {
-			shape.move(0.0f, 30.0f);
-		}
-	}
+  Game() : _window(VideoMode(1920, 1080), "SFML Game") {
+      _window.setFramerateLimit(60);
+  } 
+  void setWindow() {
+	  _window.clear(Color::White);
+	  _window.draw(shape);
+	  _window.display();
+  }
+  RenderWindow* getWindow() {
+      return &_window;
+  }
+  void setPlayer() {  
+     shape.setRadius(50.0f);  
+     shape.setFillColor(Color::Blue); 
+     shape.setPosition(320.0f - 50, 240.0f - 50);  
+  }
+  CircleShape getPlayer() {
+	  shape.setFillColor(Color::Blue);
+	  return shape;
+  }
 
-};
+};  
 
-int main()
-{
-	// Create a window
-    RenderWindow window(VideoMode(1920, 1080), "WIndow");
-    Style::Fullscreen;
-	CircleShape shape(50.0f);
-	shape.setFillColor(Color::Green);
-	shape.setPosition(320.0f - 50, 800.0f - 50);
-	Player player(shape);
-	while (window.isOpen())
-	{
-
-		window.draw(shape);
-		window.display();
-		window.clear(Color::Black);
-
+int main() {  
+	Game game;
+	game.setPlayer();
+	
+	while (game._window.isOpen()) {
 		Event event;
-		while (window.pollEvent(event)) {
 
+		while (game._window.pollEvent(event)) {
+			if (Keyboard::isKeyPressed(Keyboard::Left) == true) {
+				game.getPlayer().move(30.0f, 0.0f);
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Right) == true) {
+				game.getPlayer().move(30.0f, 0.0f);
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Up) == true) {
+				game.getPlayer().move(0.0f, -30.0f);
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Down) == true) {
+				game.getPlayer().move(0.0f, 30.0f);
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Escape) == true) {
+				game.getWindow()->close();
 			switch (event.type) {
 			case Event::Closed:
 				window.close();
@@ -66,7 +69,9 @@ int main()
 			default:
 				break;
 			}
+			game.setWindow();
+			game.getWindow();
 		}
-	}
-    return 0;
+		
+	}  
 }

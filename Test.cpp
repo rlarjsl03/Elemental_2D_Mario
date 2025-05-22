e3001602e7ab92c35bd3115b25c01423abadf3bc
GunHee <405zign@naver.com>
2025-05-22 14:17:01 +0900

    test

---
+++ Test.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+
+int main() {
+	sf::RenderWindow __window;
+	__window.create(sf::VideoMode(800, 600), "SFML window");
+}

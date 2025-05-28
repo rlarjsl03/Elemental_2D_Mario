8c66e97f71d1e8266ba6565961845e0e78411b13
zhzhf <zhzhfh4121@naver.com>
2025-05-28 23:50:42 +0900

    안녕하세요 백그라운드 합쳤어요

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Window.hpp>
 #include "Player.h"
 #include "Enemy.h"
+#include "BackGround.h"
 
 using namespace sf;
 
@@ -9,6 +10,7 @@ int main() {
    RenderWindow window(VideoMode(1920, 1080), "Player Control");
    window.setFramerateLimit(60);
 
+   Background gameBackground("Background.jpg"); //백그라운드 객체 생성
    Player player;
    Enemy enemy("enemy.png", Vector2f(300.f, 0.f));  // y는 update에서 바닥 고정됨
 
@@ -25,8 +27,12 @@ int main() {
        }
 
        player.handleInput(deltaTime);  // 키 입력 처리
+       // 배경 업데이트 (예: 100 픽셀/초 속도로 스크롤)
+         // 이 scrollSpeed는 게임 캐릭터의 이동 속도에 따라 동적으로 조절할 수 있습니다.
+       gameBackground.update(deltaTime, 100.0f);
        player.update(deltaTime); // 중력 적용
        enemy.update(deltaTime, 800.f);  // groundY = 500
+       gameBackground.draw(window); // 배경 그리기
 
        // 충돌 체크
        FloatRect playerBounds = player.getSprite().getGlobalBounds();
@@ -47,6 +53,7 @@ int main() {
        }
 
        window.clear(Color::White);
+       gameBackground.draw(window); // 배경 그리기
        player.draw(window);            // 플레이어 그리기
        if (!enemy.isDead())
            enemy.draw(window);
